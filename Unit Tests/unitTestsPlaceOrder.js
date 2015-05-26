cd275309b84bca72df0b21ab88e90721a1d627dc
Tonia Michael <tonia.michael94@gmail.com>
2015-05-26 12:40:51 +0200

    added unit tests for place order

---
+++ Unit Tests/unitTestsPlaceOrder.js
@@ -0,0 +1,132 @@
+//Place order
+var placeOrderObj = require('./placeOrder');
+var profileObj=require('./profile');
+var loginObj = require('./login');
+
+it('should pass', function(done){
+	var jsonObj = {
+		employeeId="7657834657";
+		password="dummy";
+	}
+	var result = loginObj .login(jsonObj) ;
+		 if(result=="successful login"){
+            throw 'true';
+        }else 
+		done();
+ });
+
+it('should fail', function(done){
+	var jsonObj = {
+		employeeId="7657834657";
+		password="";
+	}
+	var result = loginObj .login(jsonObj) ;
+		 if(result=="unsuccessful login"){
+            throw 'false';
+        }else 
+		done();
+ });
+  
+ it('should pass', function(done){
+        var jsonObj = {
+		productName="sandwich";
+        }
+        var result = checkProductAvailability()(jsonObj);
+        if(result=="is available"){
+            throw 'true';
+        }else done();
+        //done();
+    });
+
+ //non available salad
+ it('should pass', function(done){
+        var jsonObj = {
+		productName="Greek salad";
+        }
+        var result = checkProductAvailabilit(jsonObj);
+        if(result=="is not available"){
+            throw 'true';
+        }else done();
+        //done();
+    });
+
+ it('should pass', function(done){
+        var jsonObj = {
+		employeeId="784365783467";
+		totalPrice="R59.90";
+        }
+        var result = checkLimits(jsonObj);
+        if(result=="success - within limit"){
+            throw 'true';
+        }else done();
+        //done();
+    });
+ 
+ it('should fail', function(done){
+        var jsonObj = {
+		employeeId="784365783467";
+		totalPrice="";
+        }
+        var result = checkLimits(jsonObj);
+        if(result=="error - invalid parameters"){
+            throw 'false';
+        }else done();
+        //done();
+    });
+
+ it('should fail', function(done){
+        var jsonObj = {
+		employeeId="";
+		totalPrice="";
+        }
+        var result = checkLimits(jsonObj);
+        if(result=="error - invalid parameters"){
+            throw 'false';
+        }else done();
+        //done();
+    });
+ it('should fail', function(done){
+        var jsonObj = {
+		employeeId="";
+		totalPrice="R10.00";
+        }
+        var result = checkLimits(jsonObj);
+        if(result=="error - invalid parameters"){
+            throw 'false';
+        }else done();
+        //done();
+    });
+
+ it('should pass', function(done){
+        var jsonObj = {
+		employeeId="3567832465476"; 
+		items=[{"sandwich","R25.50"},{"fanta orange","R10.00"}];
+        }
+        var result = generateBill(jsonObj);
+        if(result=="Successfully generated"){
+            throw 'true';
+        }else done(); 
+    });
+
+ it('should fail', function(done){
+        var jsonObj = {
+		employeeId=""; 
+		items=[{"sandwich","R25.50"},{"fanta orange","R10.00"}];
+        }
+        var result = generateBill(jsonObj);
+        if(result=="Unsuccessful"){
+            throw 'false';
+        }else done(); 
+    });
+
+ it('should fail', function(done){
+        var jsonObj = {
+		employeeId="474562786579"; 
+		items=[];
+        }
+        var result = generateBill(jsonObj);
+        if(result=="Unsuccessful"){
+            throw 'false';
+        }else done(); 
+    });
+
