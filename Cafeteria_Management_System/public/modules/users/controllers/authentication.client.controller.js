b569f58f038e99e686ec3a8188d309be45655ddc
Rendani Dau <u13381467@tuks.co.za>
2015-07-02 10:18:29 +0200

    Fixed confirm password

--- Cafeteria_Management_System/public/modules/users/controllers/authentication.client.controller.js
+++ Cafeteria_Management_System/public/modules/users/controllers/authentication.client.controller.js
@@ -8,15 +8,23 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		if ($scope.authentication.user) $location.path('/');
 
 		$scope.signup = function() {
-			$http.post('/auth/signup', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
+			/*
+			 *
+			 */
+			if($scope.credentials.password === $scope.credentials.confirmPassword){
+				$http.post('/auth/signup', $scope.credentials).success(function(response) {
+					// If successful we assign the response to the global user model
+					$scope.authentication.user = response;
 
-				// And redirect to the index page
-				$location.path('/');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
+					// And redirect to the index page
+					$location.path('/');
+				}).error(function(response) {
+					$scope.error = response.message;
+				});
+			}
+			else {
+					$scope.error = "Passwords do not match!"
+			}
 		};
 
 		$scope.signin = function() {
