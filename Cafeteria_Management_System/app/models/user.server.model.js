1f15b0c2f28935b5c5de464108275baee273a8f7
Tonia Michael <tonia.michael94@gmail.com>
2015-07-02 11:32:40 +0200

    added different user roles to array

--- Cafeteria_Management_System/app/models/user.server.model.js
+++ Cafeteria_Management_System/app/models/user.server.model.js
@@ -75,7 +75,7 @@ var UserSchema = new Schema({
 	roles: {
 		type: [{
 			type: String,
-			enum: ['user', 'admin']
+			enum: ['user','superuser','cashier', 'cafeteriaManager']
 		}],
 		default: ['user']
 	},
