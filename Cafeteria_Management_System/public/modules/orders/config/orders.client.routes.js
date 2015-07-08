db5c77d40585f091dc7f1b1225e12e0f335d181e
Lana <u12029522@tuks.co.za>
2015-07-08 13:21:19 +0200

    changed folders

--- Cafeteria_Management_System/public/modules/orders/config/orders.client.routes.js
+++ Cafeteria_Management_System/public/modules/orders/config/orders.client.routes.js
@@ -7,23 +7,23 @@ angular.module('orders').config(['$stateProvider',
 		$stateProvider.
 		state('listOrders', {
 			url: '/orders',
-			templateUrl: 'modules/orders/views/orders/list-orders.client.view.html'
+			templateUrl: 'modules/orders/views/menu/list-orders.client.view.html'
 		}).
 		state('createOrder', {
 			url: '/orders/create',
-			templateUrl: 'modules/orders/views/orders/create-order.client.view.html'
+			templateUrl: 'modules/orders/views/menu/create-order.client.view.html'
 		}).
 		state('viewOrder', {
 			url: '/orders/:orderId',
-			templateUrl: 'modules/orders/views/orders/view-order.client.view.html'
+			templateUrl: 'modules/orders/views/menu/view-order.client.view.html'
 		}).
 		state('menu', {
 			url: '/orders/menu',
-			templateUrl: 'modules/orders/views/orders/menu.html'
+			templateUrl: 'modules/orders/views/menu/menu.html'
 		}).
 		state('editOrder', {
 			url: '/orders/:orderId/edit',
-			templateUrl: 'modules/orders/views/orders/edit-order.client.view.html'
+			templateUrl: 'modules/orders/views/menu/edit-order.client.view.html'
 		});
 	}
 ]);
