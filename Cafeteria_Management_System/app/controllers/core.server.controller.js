5ce48ca34d43f4a35e6468de51e1380bb5a931cc
Isabel Nel <isabel.nel.isabel@gmail.com>
2015-07-09 11:16:36 +0200

    Adding canteen name on home page

--- Cafeteria_Management_System/app/controllers/core.server.controller.js
+++ Cafeteria_Management_System/app/controllers/core.server.controller.js
@@ -3,9 +3,31 @@
 /**
  * Module dependencies.
  */
+ var _ = require('lodash'),
+ 	//errorHandler = require('errors.server.controller'),
+ 	mongoose = require('mongoose'),
+ 	passport = require('passport'),
+ 	User = mongoose.model('User'),
+ 	Config = mongoose.model('Config');
+
 exports.index = function(req, res) {
 	res.render('index', {
 		user: req.user || null,
 		request: req
 	});
+};
+
+exports.loadCanteenInfo = function(req, res) {
+
+	Config.findOne({name: 'Canteen name' }, function(err, canteenName){
+		if(err || !canteenName) return res.status(400).send({message: 'canteen name not found' });
+		else {
+			res.status(200).send({message: canteenName});
+			var v = canteenName;
+			console.log(v);
+		}
+
+	});
+
+	//console.log(canteenName);
 };
--- Cafeteria_Management_System/public/modules/core/controllers/header.client.controller.js
+++ Cafeteria_Management_System/public/modules/core/controllers/header.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', '$http', 'Authentication', 'Menus',
+	function($scope,  $http, Authentication, Menus) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
@@ -9,6 +9,24 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
 		};
+
+		$scope.loadCanteenInfo = function(){
+			console.log('in load canteen info...');
+			$http.get('/loadCanteenInfo').success(function(response) {
+				// If successful show success message and clear form
+			$scope.canteenName = response.message.value;
+			console.log(response.message.value);
+
+			}).error(function(response) {
+				$scope.canteenName = 'Canteen Name (undifined)';
+			});
+			console.log($scope.canteenName);
+		};
+
+
+
+
+
 
 		// Collapsing the menu after navigation
 		$scope.$on('$stateChangeSuccess', function() {
