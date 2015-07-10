9ebf46c694e8dacffe136bdf0317532b566df58b
Isabel Nel <isabel.nel.isabel@gmail.com>
2015-07-10 13:26:33 +0200

    Menu items are being displayd from datbase

--- Cafeteria_Management_System/public/modules/orders/controllers/menuitems.client.controller.js
+++ Cafeteria_Management_System/public/modules/orders/controllers/menuitems.client.controller.js
@@ -5,29 +5,35 @@ angular.module('menuItems').controller('MenuItemsController', ['$scope', '$http'
 	function($scope, $http, $stateParams, $location, Authentication, MenuItems) {
 		$scope.authentication = Authentication;
 
-		//display menu item
+		//Filter Menu items - meals
+		$scope.meals = function (row) {
+        return (angular.lowercase(row.category).indexOf('Meals') !== -1);
+    };
+
+		//Filter Menu items for snacks
+		$scope.snacks = function (row) {
+        return (angular.lowercase(row.category).indexOf('Snacks') !== -1);
+    };
+	
+
+
+		//get menu items from database on the server side
 		$scope.loadMenuItems = function(){
-			console.log('in load menu items ...');
 			$http.get('/loadMenuItems').success(function(response) {
 				// If successful show success message and clear form
-			//	var itemNames = responce.message['category'];
-				//console.log('itemNames: ' + itemNames);
-				console.log('responce = ' + response.message);
+		  //console.log('responce = ' + response.message); // testing
 			$scope.menuItems = response.message;
 			var itemsArray    = new Array();
 			var counter = 0;
 
 			for(var itemName in response.message){
-				console.log(itemName + " = " + response.message[itemName].itemName);
-				itemsArray[counter].itemName = response.message[itemName].itemName
+				//console.log(itemName + " = " + response.message[itemName].itemName);// testing
+				itemsArray[counter] = response.message[itemName]
 			  counter++;
 			}
-			$scope.menuItems = itemsArray;//itemsArray; "order.created | date:'medium'"
-		  	console.log('array size = ' + itemsArray.length);
-			 for (var i=0; i<itemsArray.length; i++) {
-			//  document.write("<tr><td>" + i + " is:</td>");
-			//  document.write("<td>" + itemsArray[i] + "</td></tr>");
-			}
+
+			$scope.menuItems = itemsArray;
+		  //	console.log('array size = ' + itemsArray.length);// testing
 
 				}).error(function(response) {
 				$scope.menuItems = 'Error loading menu Items';
