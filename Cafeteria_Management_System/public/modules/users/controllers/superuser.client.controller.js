23599dffe55362b713d8499c824ba708d27e07c4
Rendani Dau <u13381467@tuks.co.za>
2015-07-14 10:35:05 +0200

    Fixed refresh for assigning superuser

--- Cafeteria_Management_System/public/modules/users/controllers/superuser.client.controller.js
+++ Cafeteria_Management_System/public/modules/users/controllers/superuser.client.controller.js
@@ -1,73 +1,79 @@
 'use strict';
 
-angular.module('users').controller('superuserController', ['$scope', '$http', '$location', 'Users', 'Authentication',
-	function($scope, $http, $location, Users, Authentication) {
+angular.module('users').controller('superuserController', ['$scope', '$http', '$location', '$window', 'Users', 'Authentication',
+	function($scope, $http, $location, $window, Users, Authentication) {
 		$scope.user = Authentication.user;
 
-    // If user is not signed in then redirect back home
+		// If user is not signed in then redirect back home
 		if (!$scope.user) $location.path('/');
 
-    // Assign a role to user
-    $scope.assignRoles = function(isValid) {
-      if (isValid) {
-        $scope.success = $scope.error = null;
-        var reqObj = {userID: $scope.emp_id, role: $scope.role};
+		// Assign a role to user
+		$scope.assignRoles = function(isValid) {
+		  if (isValid) {
+			$scope.success = $scope.error = null;
+			var reqObj = {userID: $scope.emp_id, role: $scope.role};
 
-        $http.post('/users/superuserAssignRoles', reqObj).success(function(response) {
-          // If successful show success message and clear form
-        $scope.success = response.message;
-        }).error(function(response) {
-          $scope.error = response.message;
-        });
-        }
-      };
-
-			//Set system limit
-			$scope.setSystemWideLimit = function(isValid){
-				if(isValid){
-					$scope.success = $scope.error = null;
-					$scope.successTwo = $scope.errorTwo = null;
-
-					var reqObj = {name: 'System wide limit', value: $scope.limit};
-					//console.log($scope.limit);
-					$http.post('users/superuserSetSystemWideLimit', reqObj).success(function(response){
-						$scope.successTwo = response.message;
-					}).error(function(response){
-						$scope.errorTwo = response.message;
-					});
+			$http.post('/users/superuserAssignRoles', reqObj).success(function(response) {
+				if(response.message === 'SU Changed'){
+					$window.location.href = '/';
 				}
-			};
-
-			//Set canteen name
-			$scope.setCanteenName = function(isValid){
-				if(isValid){
-					$scope.success = $scope.error = null;
-					$scope.successTwo = $scope.errorTwo = null;
-
-					var reqObj = {name: 'Canteen name', value: $scope.canteenName};
-					//console.log($scope.canteenName);
-					$http.post('users/superuserSetCanteenName', reqObj).success(function(response){
-						$scope.successThree = response.message;
-					}).error(function(response){
-						$scope.errorThree = response.message;
-					});
+				else{
+					// If successful show success message and clear form
+					$scope.success = response.message;
+					$scope.emp_id = $scope.role = null;
 				}
-			};
+			}).error(function(response) {
+			  $scope.error = response.message;
+			});
+			}
+		  };
 
-            //Set image
-            $scope.setImage = function(isValid){
-                if(isValid){
-                    $scope.success = $scope.error = null;
-                    $scope.successTwo = $scope.errorTwo = null;
+		//Set system limit
+		$scope.setSystemWideLimit = function(isValid){
+			if(isValid){
+				$scope.success = $scope.error = null;
+				$scope.successTwo = $scope.errorTwo = null;
 
-                    var reqObj = {name: 'Canteen name', value: $scope.image};
-                    //console.log($scope.canteenName);
-                    $http.post('users/superuserSetCanteenName', reqObj).success(function(response){
-                        $scope.successThree = response.message;
-                    }).error(function(response){
-                        $scope.errorThree = response.message;
-                    });
-                }
-            };
+				var reqObj = {name: 'System wide limit', value: $scope.limit};
+				//console.log($scope.limit);
+				$http.post('users/superuserSetSystemWideLimit', reqObj).success(function(response){
+					$scope.successTwo = response.message;
+				}).error(function(response){
+					$scope.errorTwo = response.message;
+				});
+			}
+		};
+
+		//Set canteen name
+		$scope.setCanteenName = function(isValid){
+			if(isValid){
+				$scope.success = $scope.error = null;
+				$scope.successTwo = $scope.errorTwo = null;
+
+				var reqObj = {name: 'Canteen name', value: $scope.canteenName};
+				//console.log($scope.canteenName);
+				$http.post('users/superuserSetCanteenName', reqObj).success(function(response){
+					$scope.successThree = response.message;
+				}).error(function(response){
+					$scope.errorThree = response.message;
+				});
+			}
+		};
+
+		//Set image
+		$scope.setImage = function(isValid){
+			if(isValid){
+				$scope.success = $scope.error = null;
+				$scope.successTwo = $scope.errorTwo = null;
+
+				var reqObj = {name: 'Canteen name', value: $scope.image};
+				//console.log($scope.canteenName);
+				$http.post('users/superuserSetCanteenName', reqObj).success(function(response){
+					$scope.successThree = response.message;
+				}).error(function(response){
+					$scope.errorThree = response.message;
+				});
+			}
+		};
     }
 ]);
