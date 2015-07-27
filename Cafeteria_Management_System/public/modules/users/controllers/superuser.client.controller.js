260810c5bb1cc708fa7649a4c58d45b3d7840ae9
root <tonia.michael94@gmail.com>
2015-07-27 12:31:29 +0200

    Added popup confirm dialog functionality with Semaka

--- Cafeteria_Management_System/public/modules/users/controllers/superuser.client.controller.js
+++ Cafeteria_Management_System/public/modules/users/controllers/superuser.client.controller.js
@@ -7,24 +7,40 @@ angular.module('users').controller('superuserController', ['$scope', '$http', '$
 		// If user is not signed in then redirect back home
 		if (!$scope.user) $location.path('/');
 
+       /* exports.really = function(req, res) {
+            $scope.r = confirm("Press a button");
+            if ($scope.r == true) {
+                $scope.x = "You pressed OK!";
+            } else {
+                $scope.x = "You pressed Cancel!";
+            }
+        };*/
+
 		// Assign a role to user - as the super user
 		$scope.assignRoles = function(isValid) {
 		  if (isValid) {
 			$scope.success = $scope.error = null;
 			var reqObj = {userID: $scope.emp_id, role: $scope.role};
+              $scope.r = $window.confirm("Are you sure?");
 
-			$http.post('/users/superuserAssignRoles', reqObj).success(function(response) {
-				if(response.message === 'SU Changed'){
-					$window.location.href = '/';
-				}
-				else{
-					// If successful show success message and clear form
-					$scope.success = response.message;
-					$scope.emp_id = $scope.role = null;
-				}
-			}).error(function(response) {
-			  $scope.error = response.message;
-			});
+              if($scope.r === true) {
+                  $http.post('/users/superuserAssignRoles', reqObj).success(function (response) {
+                      if (response.message === 'SU Changed') {
+                          $window.location.href = '/';
+                      }
+
+                      // If successful show success message and clear form
+                      $scope.success = response.message;
+                      $scope.emp_id = $scope.role = null;
+
+
+                  }).error(function (response) {
+                      $scope.error = response.message;
+                  });
+              }
+              else{
+                  $scope.emp_id = $scope.role = null;
+              }
 			}
 		  };
 
@@ -33,19 +49,25 @@ angular.module('users').controller('superuserController', ['$scope', '$http', '$
 				if (isValid) {
 				$scope.success = $scope.error = null;
 				var reqObj = {userID: $scope.emp_id, role: $scope.role};
+                $scope.r = $window.confirm("Are you sure?");
 
-				$http.post('/users/adminUserAssignRoles', reqObj).success(function(response) {
-					if(response.message === 'Admin Changed'){
-						$window.location.href = '/';
-					}
-					else{
-						// If successful show success message and clear form
-						$scope.success = response.message;
-						$scope.emp_id = $scope.role = null;
-					}
-				}).error(function(response) {
-					$scope.error = response.message;
-				});
+                if($scope.r === true) {
+                    $http.post('/users/adminUserAssignRoles', reqObj).success(function (response) {
+                        if (response.message === 'Admin Changed') {
+                            $window.location.href = '/';
+                        }
+                        else {
+                            // If successful show success message and clear form
+                            $scope.success = response.message;
+                            $scope.emp_id = $scope.role = null;
+                        }
+                    }).error(function (response) {
+                        $scope.error = response.message;
+                    });
+                }
+                else{
+                    $scope.emp_id = $scope.role = null;
+                }
 				}
 				};
 
@@ -54,13 +76,20 @@ angular.module('users').controller('superuserController', ['$scope', '$http', '$
             if (isValid) {
                 $scope.successOne = $scope.errorOne = null;
                 var reqObj = {currentUserID: $scope.currentEmp_id, newUserID: $scope.newEmp_id};
-                $http.post('/users/superuserChangeEmployeeID', reqObj).success(function(response) {
-                    // If successful show success message and clear form
-                    $scope.successOne = response.message;
-                    $scope.currentEmp_id = $scope.newEmp_id = null;
-                }).error(function(response) {
-                    $scope.errorOne = response.message;
-                });
+                $scope.r = $window.confirm("Are you sure?");
+
+                if($scope.r === true) {
+                    $http.post('/users/superuserChangeEmployeeID', reqObj).success(function (response) {
+                        // If successful show success message and clear form
+                        $scope.successOne = response.message;
+                        $scope.currentEmp_id = $scope.newEmp_id = null;
+                    }).error(function (response) {
+                        $scope.errorOne = response.message;
+                    });
+                }
+                else{
+                    $scope.emp_id = $scope.role = null;
+                }
             }
         };
 
@@ -87,19 +116,26 @@ angular.module('users').controller('superuserController', ['$scope', '$http', '$
                 }
         };
 
-		//Set system limit
+        //Set system limit
 		$scope.setSystemWideLimit = function(isValid){
 			if(isValid){
 				$scope.success = $scope.error = null;
 				$scope.successTwo = $scope.errorTwo = null;
 
 				var reqObj = {name: 'System wide limit', value: $scope.limit};
-				//console.log($scope.limit);
-				$http.post('users/superuserSetSystemWideLimit', reqObj).success(function(response){
-					$scope.successTwo = response.message;
-				}).error(function(response){
-					$scope.errorTwo = response.message;
-				});
+                $scope.r = $window.confirm("Are you sure?");
+
+                if($scope.r === true) {
+                    //console.log($scope.limit);
+                    $http.post('users/superuserSetSystemWideLimit', reqObj).success(function (response) {
+                        $scope.successTwo = response.message;
+                    }).error(function (response) {
+                        $scope.errorTwo = response.message;
+                    });
+                }
+                else{
+                    $scope.emp_id = $scope.role = null;
+                }
 			}
 		};
 
@@ -110,12 +146,19 @@ angular.module('users').controller('superuserController', ['$scope', '$http', '$
 				$scope.successTwo = $scope.errorTwo = null;
 
 				var reqObj = {name: 'Canteen name', value: $scope.canteenName};
-				//console.log($scope.canteenName);
-				$http.post('users/superuserSetCanteenName', reqObj).success(function(response){
-					$scope.successThree = response.message;
-				}).error(function(response){
-					$scope.errorThree = response.message;
-				});
+                $scope.r = $window.confirm("Are you sure?");
+
+                if($scope.r === true) {
+                    //console.log($scope.canteenName);
+                    $http.post('users/superuserSetCanteenName', reqObj).success(function (response) {
+                        $scope.successThree = response.message;
+                    }).error(function (response) {
+                        $scope.errorThree = response.message;
+                    });
+                }
+                else{
+                    $scope.emp_id = $scope.role = null;
+                }
 			}
 		};
     }
