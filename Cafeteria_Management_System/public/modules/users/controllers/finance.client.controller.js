7cd76edef1e7428aa59f041408d17f25e9be0683
semakamalapane <malapanesemaka@gmail.com>
2015-08-19 17:14:29 +0200

    added finance functionality to the client controller

---
+++ Cafeteria_Management_System/public/modules/users/controllers/finance.client.controller.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('users').controller('FinanceController', ['$scope', '$http', '$location', '$stateParams', 'Authentication',
+    function($scope, $http, $location, $stateParams, Authentication) {
+        //$scope.user = Authentication.user;
+        $scope.authentication = Authentication;
+
+        // If user is not signed in then redirect back home
+        if (!$scope.authentication) $location.path('/');
+
+        // Search a user profile
+        /*$scope.searchEmployee = function(isValid) {
+            if(isValid){
+                $scope.success = $scope.error = null;
+
+                var reqObj = {username: $scope.username};
+                    $http.post('/orders/getUserOrders', reqObj).success(function(response){
+                        $scope.orders = response.message;
+                    }).error(function(response){
+                        $scope.error = response.message;
+                    });
+            }
+        };*/
+
+        $scope.getUserOrders = function(isValid){
+            var reqObj = {username: $scope.username};
+            $http.post('/orders/getUserOrders', reqObj).success(function(response){
+                console.log('success' + response);
+                console.log('success2' + response.message);
+                $scope.orders = response.message;
+            }).error(function(response){
+                console.log('error' + response.message);
+                $scope.error = response.message;
+            });
+        };
+
+        $scope.checkUser = function(){
+            if(!Authentication.user){
+                $location.path('/');
+            }
+            else if(Authentication.user.roles[0] !== 'finance'){
+                $location.path('/');
+            }
+        };
+    }
+]);
