aacef14ef83046969920f6bfa7bd8a4169ab0e78
Rendani Dau <u13381467@tuks.co.za>
2015-09-10 09:15:51 +0200

    testing out reporting

--- Cafeteria_Management_System/app/controllers/users/users.finance.server.controller.js
+++ Cafeteria_Management_System/app/controllers/users/users.finance.server.controller.js
@@ -11,14 +11,32 @@ var _ = require('lodash'),
 	Config = mongoose.model('Config'),
     formidable = require('formidable'),
     fs = require('fs'),
+	path = require('path'),
 	configs = require('../../../config/config'),
 	nodemailer = require('nodemailer'),
 	jsreport = require('jsreport');
 	
+	
+
+	
 exports.generateReport = function(req, res){
 	console.log('in generate report');
-	jsreport.render("<h1>Hello world</h1>").then(function(out) {
-		console.log('in render function');
-		out.stream.pipe(res);
-	});
+		console.log('rendering');
+		var sample = fs.readFileSync(path.resolve(__dirname, '../../reportTemplates/sample.html'), 'utf8');
+		jsreport.render({
+			template:{ content: sample},
+			data: {
+				number: '1',
+				date: '01-01-2014',
+				dueDate: '01-20-2014',
+				to: { name: 'John Lemon' , description: '', mail: 'john.lemon@finmail.com'},
+				items : [ { name: 'Designing online portal', quantity: 1, price : 1000}, 
+							{ name: 'Implementing online portal', quantity: 1, price : 8000},
+							{ name: 'Testing online portal', quantity: 1, price : 2000} ]
+			}
+		}).then(function(out) {
+			console.log('in render function');
+			out.stream.pipe(res);
+		});
+	
 };
